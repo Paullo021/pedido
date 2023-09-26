67e3731410627e9bb1784eb4495821e312812a47
Paullo021 <146101571+Paullo021@users.noreply.github.com>
2023-09-26 10:50:18 -0300

    subi os arquivos

---
+++ script.js
@@ -0,0 +1,15 @@
+    function sim(){
+     alert("Você aceitou ficar comigo! <3")
+    }
+
+    function desvia(t){
+     var btn = t;
+     btn.style.position = 'absolute';
+     btn.style.bottom = geraPosicao(10, 90);
+     btn.style.left = geraPosicao(10, 90);;
+     console.log("Eu não aceito não.")
+    }
+
+    function geraPosicao(min, max) {
+        return (Math.random() * (max - min) + min) + "%";
+    }
